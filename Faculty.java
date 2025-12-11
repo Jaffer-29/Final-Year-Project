e79ceda42f4a902e9992473d6fadbc1edbcea0be
Jaffer-29 <m.jaffer2904@gmail.com>
2025-12-11 21:03:38 +0500

    Push Faculty Class Code

---
+++ Faculty.java
@@ -0,0 +1,126 @@
+//Faculty Code that display the Info of Other Staff Members
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Faculty  extends JFrame {
+    JButton back;
+    JComboBox<String> facBox;
+    JLabel fName, fRole, fDepartment, fShift, fWeekDays, fContact, fLocation;
+
+    FacultyInfo[] fac = {
+            new FacultyInfo("Select Employee","","","","","",""),
+            new FacultyInfo("Abbas Khan", "Security Guard","Security Department", "8am to 8pm","Monday to Sunday", "0301-4412233", "Main Gate 1"),
+            new FacultyInfo("Shahid Khan", "Security Guard","Security Department", "8pm to 8am (Night Shift)","Monday to Sunday", "0302-6644511", "Emergency Gate"),
+            new FacultyInfo("Shahab Abbas", "Admin Manager","Administration Department", "9am to 6pm","Monday to Saturday", "0321-5566778", "Admin Block"),
+            new FacultyInfo("Sara Ali", "Receptionist","Administration Department", "8am to 4pm","Monday to Saturday", "0333-1122334", "Main Reception Desk"),
+            new FacultyInfo("Imran", "Office Boy","Administration Department", "8am to 4pm","Monday to Saturday", "0301-2244668", "Admin Office"),
+            new FacultyInfo("Ahmad Abdullah", "Office Boy","General Services", "9am to 5pm","Monday to Saturday", "0315-6677889", "ICU and OPD"),
+            new FacultyInfo("Kamran Ali", "Office Boy","General Services", "9am to 5pm","Monday to Saturday", "0345-2213345", "Surgery Block"),
+            new FacultyInfo("Hamza Nasir", "X-Ray Technician","Radiology Department", "10am to 6pm","Monday to Saturday", "0314-9001122", "Radiology Lab"),
+            new FacultyInfo("Muhammad Usman", "Lab Technician","Pathology Department", "9am to 5pm","Monday to Saturday", "0342-7711223", "Pathology Lab")
+    };
+
+    Faculty(){
+        JLabel title = new JLabel("\"Faculty's Info Panel\"");
+        title.setBounds(200,18,330,25);
+        title.setFont(new Font("Roboto", Font.BOLD, 23));
+        title.setForeground(Color.BLACK);
+        add(title);
+
+        String[] facInfo =new String[fac.length];
+        for(int i = 0; i < fac.length;i++){
+            facInfo[i] = fac[i].name;
+        }
+        facBox = new JComboBox<>(facInfo);
+        facBox.setBounds(10, 55, 150, 20);
+        facBox.setFocusable(false);
+        facBox.addActionListener(ae -> updateLabels(facBox.getSelectedIndex()));
+        add(facBox);
+
+
+        addStaticLabel("Employee Name", 35, 95);
+        addStaticLabel("Role", 35, 140);
+        addStaticLabel("Working Department", 35, 180);
+        addStaticLabel("Shift Hours", 35, 215);
+        addStaticLabel("Working Days", 35, 255);
+        addStaticLabel("Contact Info", 35, 295);
+        addStaticLabel("Location", 35, 335);
+
+        fName = createLabel(271, 95);
+        fRole = createLabel(271, 140);
+        fDepartment = createLabel(271, 180);
+        fShift = createLabel(271, 215);
+        fWeekDays = createLabel(271, 255);
+        fContact = createLabel(271, 295);
+        fLocation = createLabel(271, 335);
+
+
+        back= new JButton("Back");
+        back.setForeground(Color.WHITE);
+        back.setBackground(Color.BLACK);
+        back.setBounds(260,430,120,30);
+        back.setFocusPainted(false);
+        back.setBorder(null);
+        back.addActionListener(ae->{
+            dispose();
+        });
+        add(back);
+
+        ImageIcon icon = new ImageIcon("E:\\Side Code\\src\\Icon.png");
+        setIconImage(icon.getImage());
+        setLayout(null);
+        getContentPane().setBackground(new Color(129, 133, 137	 ));
+        setSize(650,550);
+        setLocation(300,285);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+    }
+    public void addStaticLabel(String text,int x,int y){
+        JLabel label = new JLabel(text);
+        label.setBounds(x, y, 200, 30);
+        label.setFont(new Font("Roboto", Font.BOLD, 14));
+        label.setForeground(Color.WHITE);
+        add(label);
+    }
+    public JLabel createLabel(int x, int y) {
+        JLabel label = new JLabel();
+        label.setBounds(x, y, 300, 30);
+        label.setFont(new Font("Roboto", Font.BOLD, 14));
+        label.setForeground(Color.WHITE);
+        add(label);
+        return label;
+    }
+
+
+    public void updateLabels(int index) {
+        FacultyInfo f = fac[index];
+        fName.setText(f.name);
+        fRole.setText(f.role);
+        fDepartment.setText(f.department);
+        fShift.setText(f.shiftHours);
+        fWeekDays.setText(f.workdays);
+        fContact.setText(f.contact);
+        fLocation.setText(f.location);
+    }
+}
+
+
+class FacultyInfo{
+
+    String name,role, department,shiftHours,workdays,contact,location;
+
+    FacultyInfo(String name,String role,  String department, String shiftHours,
+                String workdays, String contact,String location){
+
+        this.name=name;
+        this.role=role;
+        this.department=department;
+        this.shiftHours=shiftHours;
+        this.workdays=workdays;
+        this.contact=contact;
+        this.location=location;
+
+    }
+}
