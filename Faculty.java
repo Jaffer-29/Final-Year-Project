bf4676a343b6a48c592759ca2fb33b399d584d7a
Jaffer-29 <m.jaffer2904@gmail.com>
2025-12-13 19:58:09 +0500

    Change the Code Styling. Make it more clean and adjust the items. Code seem to be neat now

--- Faculty.java
+++ Faculty.java
@@ -10,15 +10,18 @@ public class Faculty  extends JFrame {
 
     FacultyInfo[] fac = {
             new FacultyInfo("Select Employee","","","","","",""),
-            new FacultyInfo("Abbas Khan", "Security Guard","Security Department", "8am to 8pm","Monday to Sunday", "0301-4412233", "Main Gate 1"),
-            new FacultyInfo("Shahid Khan", "Security Guard","Security Department", "8pm to 8am (Night Shift)","Monday to Sunday", "0302-6644511", "Emergency Gate"),
-            new FacultyInfo("Shahab Abbas", "Admin Manager","Administration Department", "9am to 6pm","Monday to Saturday", "0321-5566778", "Admin Block"),
-            new FacultyInfo("Sara Ali", "Receptionist","Administration Department", "8am to 4pm","Monday to Saturday", "0333-1122334", "Main Reception Desk"),
-            new FacultyInfo("Imran", "Office Boy","Administration Department", "8am to 4pm","Monday to Saturday", "0301-2244668", "Admin Office"),
-            new FacultyInfo("Ahmad Abdullah", "Office Boy","General Services", "9am to 5pm","Monday to Saturday", "0315-6677889", "ICU and OPD"),
+            new FacultyInfo("Shahab Abbas", "Admin Manager","Administration Department", "9:30 am to 6pm","Monday to Saturday", "0321-5566778", "Admin Block"),
+            new FacultyInfo("Sara Ali", "Receptionist","Administration Department", "9am to 4pm","Monday to Saturday", "0333-1122334", "Main Reception Desk"),
+            new FacultyInfo("Imran", "Office Boy","Administration Department", "9am to 5pm","Monday to Saturday", "0301-2244668", "Admin Office"),
+            new FacultyInfo("Ahmad Abdullah", "Office Boy","General Services", "9am to 7:30pm","Monday to Saturday", "0315-6677889", "ICU and OPD"),
             new FacultyInfo("Kamran Ali", "Office Boy","General Services", "9am to 5pm","Monday to Saturday", "0345-2213345", "Surgery Block"),
+            new FacultyInfo("Shah Jahan", "Nurse","Assistant Staff", "9am to 5pm (reschedule on call)","Monday to Saturday (Sunday Twice a Month)", "0334-451223", "ER and OPD"),
+            new FacultyInfo("Atif Ahmed", "Nurse","Assistant Staff", "9am to 5pm (reschedule on call)","Monday to Saturday (Sunday Twice a Month)", "0342-3451223", "ER and OPD"),
+            new FacultyInfo("Nadeem", "Surgical Technician","Assistant Staff", "10am to 4pm","Monday to Saturday", "0334-451223", "ICU and OT"),
             new FacultyInfo("Hamza Nasir", "X-Ray Technician","Radiology Department", "10am to 6pm","Monday to Saturday", "0314-9001122", "Radiology Lab"),
-            new FacultyInfo("Muhammad Usman", "Lab Technician","Pathology Department", "9am to 5pm","Monday to Saturday", "0342-7711223", "Pathology Lab")
+            new FacultyInfo("Muhammad Usman", "Lab Technician","Pathology Department", "9am to 5pm","Monday to Saturday", "0342-7711223", "Pathology Lab"),
+            new FacultyInfo("Abbas Khan", "Security Guard","Security Department", "8am to 8pm","Monday to Sunday", "0301-4412233", "Main Gate 1"),
+            new FacultyInfo("Sarfraz Khan", "Security Guard","Security Department", "8pm to 8am (Night Shift)","Monday to Sunday", "0302-6644511", "Emergency Gate")
     };
 
     Faculty(){
@@ -35,7 +38,9 @@ public class Faculty  extends JFrame {
         facBox = new JComboBox<>(facInfo);
         facBox.setBounds(10, 55, 150, 20);
         facBox.setFocusable(false);
-        facBox.addActionListener(ae -> updateLabels(facBox.getSelectedIndex()));
+        facBox.addActionListener(ae ->
+                updateLabels(facBox.getSelectedIndex()
+                ));
         add(facBox);
 
 
@@ -67,16 +72,18 @@ public class Faculty  extends JFrame {
         });
         add(back);
 
-        ImageIcon icon = new ImageIcon("E:\\Side Code\\src\\Icon.png");
+        ImageIcon icon = new ImageIcon("E:\\2nd Semester\\src\\Icon.png");
         setIconImage(icon.getImage());
         setLayout(null);
         getContentPane().setBackground(new Color(129, 133, 137	 ));
         setSize(650,550);
         setLocation(300,285);
+        setTitle("Faculty Information");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setResizable(false);
         setVisible(true);
     }
+
     public void addStaticLabel(String text,int x,int y){
         JLabel label = new JLabel(text);
         label.setBounds(x, y, 200, 30);
@@ -84,6 +91,7 @@ public class Faculty  extends JFrame {
         label.setForeground(Color.WHITE);
         add(label);
     }
+
     public JLabel createLabel(int x, int y) {
         JLabel label = new JLabel();
         label.setBounds(x, y, 300, 30);
@@ -92,7 +100,6 @@ public class Faculty  extends JFrame {
         add(label);
         return label;
     }
-
 
     public void updateLabels(int index) {
         FacultyInfo f = fac[index];
@@ -105,7 +112,6 @@ public class Faculty  extends JFrame {
         fLocation.setText(f.location);
     }
 }
-
 
 class FacultyInfo{
 
